74d6e7430dc5f2355b75ff3533c5d24e0d4506a7
Geovani <geovanimendoza.h@gmail.com>
2020-12-18 14:51:21 -0400

    refactor: databinding

--- app/src/main/java/com/udacity/asteroidradar/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/screens/detail/DetailFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar.detail
+package com.udacity.asteroidradar.screens.detail
 
 
 import android.os.Bundle
--- app/src/main/java/com/udacity/asteroidradar/screens/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/screens/main/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar.main
+package com.udacity.asteroidradar.screens.main
 
 import androidx.lifecycle.ViewModel
 
